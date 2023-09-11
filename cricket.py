9894e47c49a557a0889502b2221a8df769e935fe
Shaurya144 <chundawatshaurya@gmail.com>
2023-09-11 19:10:44 +0100

    Update cricket.py

--- cricket.py
+++ cricket.py
@@ -1,4 +1,4 @@
-overs = input("HI there which over is it? please type an integer and once done please type: done").lower()
+overs = input("HI there and welcome to this cricket game: Please click enter once you're ready! : ")
 OverOne = []
 OverTwo = []
 OverThree = []
@@ -7,40 +7,57 @@ OverFive = []
 
 while overs != "done":
     over = 0
+    i = 0
     if over == 0:
-        for i in range(5):
+        while i <= 5:
             print("This is over One")
             score = int(input("Score for Over ONE : "))
             OverOne.append(score)
-        over += 1
+            i += 1
         print("Over One: " + str(OverOne))
-    elif over == 1:
-        for i in range(5):
+        i = 0
+        over += 1
+    if over == 1:
+        while i <= 5:
             print("This is over Two")
             score = int(input("Score for Over Two : "))
             OverTwo.append(score)
+            i += 1
         over += 1
-        print("Over Two: " + OverTwo)   
-    elif over == 2:
-        for i in range(5):
+        i = 0
+        print("Over Two: " + str(OverTwo))   
+    if over == 2:
+        while i <= 5:
             print("This is over Three")
             score = int(input("Score for Over Three : "))
             OverThree.append(score)
+            i += 1
         over += 1
-        print("Over Three: " + OverThree)  
-    elif over == 3:
-        for i in range(5):
+        i = 0
+        print("Over Three: " + str(OverThree))  
+    if over == 3:
+        while i <= 5:
             print("This is over Four")
             score = int(input("Score for Over Four : "))
             OverFour.append(score)
+            i += 1
         over += 1
-        print("Over Four: " + OverFour)  
-    elif over == 4:
-        for i in range(5):
+        i = 0
+        print("Over Four: " + str(OverFour))  
+    if over == 4:
+        while i <= 5:
             print("This is over Two")
             score = int(input("Score for Over Five : "))
             OverFive.append(score)
+            i += 1
         over += 1
-        print("Over Five: " + OverFive) 
-    else:
-        print("The final score was : " + str(OverOne + OverTwo + OverThree + OverFive + OverFour) + " runs.") 
+        i = 0
+        print("Over Five: " + str(OverFive)) 
+        break
+
+print("Over One: " + str(OverOne))
+print("Over Two: " + str(OverTwo))   
+print("Over Three: " + str(OverThree))  
+print("Over Four: " + str(OverFour))
+print("Over Five: " + str(OverFive)) 
+print("Thanks for playing!!!")
